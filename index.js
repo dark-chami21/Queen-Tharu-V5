deb192222fe0239f405f967318d858a67121e122
Chamindu Indula <githubbot17@gmail.com>
2024-10-12 19:00:24 +0530

    ❤️Queen Tharu v2❤️

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '/'
+const prefix = '.'
 
 const ownerNumber = ['94742952930']
 
