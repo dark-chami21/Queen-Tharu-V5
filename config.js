911eb36f2662c53256824dce3a8ae766e299cebb
Chamindu Indula <githubbot17@gmail.com>
2024-10-14 17:59:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||"I6VTnJyb#jrUDg1QVFahJN5mQdomp2Ln9EgcW67e09XDflXpddYQ",
+SESSION_ID: process.env.SESSION_ID ||"3ColWC5Z#rRtwt-pUZJMWcKtT70VpHJQHeGWoIsMbqxfn6_LgdTg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/YhYStZ8/IMG-20240923-WA0006.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HII. I AM QUEEN THARU WHATSAPP BOT OWNER BY CHAMI",
+ALIVE_MSG: process.env.ALIVE_MSG || "HII. I AM QUEEN_THARU_V➄ WHATSAPP BOT OWNER BY CHAMI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "ture",
 };
