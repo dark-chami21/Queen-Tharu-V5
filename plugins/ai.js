939533dbea2f8bdf56a3067307cb0cab0950289a
Chamindu Indula <githubbot17@gmail.com>
2024-10-15 19:44:22 +0530

    QUEEN_THARU_V➄

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,3 +1,17 @@
+/*
+
+THIS CODE WAS WRITTEN BY MR Chami ( Chami ) - ( FREELY ).
+    - @Chamindu
+GITHUB : https://github.com/dark-chami21
+------------------------------------------------------------------
+CODE FOR : @dark-chami21
+------------------------------------------------------------------
+DON'T REMOVE CREDITS !
+
+*/
+
+
+
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const { fetchJson } = require('../lib/functions')
