1e1e40aa418025cb9180392bb352f9a4be4e592a
Watsonxdboy <allmydataxd@gmail.com>
2024-10-17 08:24:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||"3ColWC5Z#rRtwt-pUZJMWcKtT70VpHJQHeGWoIsMbqxfn6_LgdTg",
+SESSION_ID: process.env.SESSION_ID ||"iXh10C6K#xq1O_-MKnkqX-LcIP-XEVG7m2aMJqRkJeXlWQ8g82t8",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/YhYStZ8/IMG-20240923-WA0006.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII. I AM QUEEN_THARU_V➄ WHATSAPP BOT OWNER BY CHAMI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "ture",
