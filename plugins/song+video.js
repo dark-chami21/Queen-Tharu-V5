0fd4379deffa657bb44fc2f6961c9f90c28a7f1b
Chamindu Indula <githubbot17@gmail.com>
2024-10-18 19:54:35 +0530

    QUEEN_THARU_V➄

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,41 +1,47 @@
 const {cmd , commands} = require('../command')
-const fg = require(`api-dylux`)
-const yts = require(`yt-search`)
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
 cmd({
     pattern: "song",
-    react: "🎵",
-    desc: "download songs",
-    category: "main",
+    react: "🎧",
+    desc: "downlod song",
+    category: "downlod",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("please give me url")
-const search =await yts(q)
-const data = search.videos[0];
-const url = data.url
 
-let desc = `
-*QUEEN THARU SONG DOWNLOADER 🎵⬇️*
+if(!q) return reply("*❌Please give me url or title*")
+const search = await yts(q)
+const deta = search.videos[0];
+const url = deta.url 
 
-title: ${data.title}
-description: ${data.description}
-time: ${data.timestamp}
-ago: ${data.ago}
-views: ${data.views}
+let desc= `
+ *©𝗠𝗨𝗦𝗜𝗖 𝐁𝐘 QUEEN_THARU_V➄*
 
-*MADE BY CHAMI*
-`
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+> ➤ *title* : ${deta.title}
 
-//download auodio=========================
+> ➤ *views* : ${deta.views}
+
+> ➤ *time* : ${deta.timestamp}
+
+> ➤ *ago* : ${deta.ago}
+
+> *©𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 CHAMINDU*`
+
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
+
+//downlod audio+ document
 
 let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
-//send auodio message=====================
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*MADE BY CHAMI*"},{quoted:mek})
+//send audio message 
+await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg",caption :"> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝚆𝙰𝚃𝚂𝙾𝙽 xᴅ*"},{quoted:mek})
+await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"audio/mpeg",fileName:deta.title + ".mp3" ,caption :"> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝚆𝙰𝚃𝚂𝙾𝙽 xᴅ*"},{quoted:mek})
+
+  
 
 }catch(e){
 console.log(e)
@@ -43,43 +49,48 @@ reply(`${e}`)
 }
 })
 
-//==============================video-dl========================================
+//========video dl=======
 
 cmd({
     pattern: "video",
     react: "🎬",
-    desc: "download videos",
-    category: "main",
+    desc: "downlod video",
+    category: "downlod",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("please give me url")
-const search =await yts(q)
-const data = search.videos[0];
-const url = data.url
 
-let desc = `
-*QUEEN THARU VIDEO DOWNLOADER 🎬⬇️*
+if(!q) return reply("❌Please give me url or title")
+const search = await yts(q)
+const deta = search.videos[0];
+const url = deta.url 
 
-title: ${data.title}
-description: ${data.description}
-time: ${data.timestamp}
-ago: ${data.ago}
-views: ${data.views}
+let desc= `
+QUEEN_THARU_V➄ 𝗩𝗜𝗗𝗘𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥
 
-*MADE BY CHAMI*
-`
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+> ➤ *title* : ${deta.title}
 
-//download video=========================
+> ➤ *views* : ${deta.views}
+
+> ➤ *time* : ${deta.timestamp}
+
+> ➤ *ago* : ${deta.ago}
+
+ ©𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 CHAMINDU `
+
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
+
+//downlod video + document 
 
 let down = await fg.ytv(url)
 let downloadUrl = down.dl_url
 
-//send video message=====================
-await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*MADE BY CHAMI*"},{quoted:mek})
+//send video  message 
+await conn.sendMessage(from,{video:{url:downloadUrl},mimetype:"video/mp4",caption :"> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝚆𝙰𝚃𝚂𝙾𝙽 xᴅ*"},{quoted:mek})
+await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"video/mp4",fileName:deta.title + ".mp4",caption :"> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝚆𝙰𝚃𝚂𝙾𝙽 xᴅ*"},{quoted:mek})
+
+  
 
 }catch(e){
 console.log(e)
