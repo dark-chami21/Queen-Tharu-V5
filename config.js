a3ba1f9190f799f3aedc86be7fe4729c24f25c23
Chamindu Indula <githubbot17@gmail.com>
2024-10-18 12:13:47 +0530

    QUEEN_THARU_V➄

--- config.js
+++ config.js
@@ -9,4 +9,7 @@ SESSION_ID: process.env.SESSION_ID ||"3ColWC5Z#rRtwt-pUZJMWcKtT70VpHJQHeGWoIsMbq
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/YhYStZ8/IMG-20240923-WA0006.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII. I AM QUEEN_THARU_V➄ WHATSAPP BOT OWNER BY CHAMI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "ture",
+AUTO_READ: process.env.AUTO_READ || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 };
